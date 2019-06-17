ccd5dcaca8a19a4180b9c225abe51e2bddcb9ea4
elias <eliaz4462@gmail.com>
2019-06-17 18:49:47 -0500

    se agrega imagen

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <div>
+
+        <img src="../img/head.png" height="709" width="1911"/>
+            <div style="position:absolute; top: 294.4px; left: 717.4px;">
+                <img src="../img/Grupo 73.png" height="145.2px" width="486.4px"/>
+            </div>
+
+            <div style="position:absolute; top: 54px; left: 120.2px;">
+                <img src="../img/Grupo 191.png" height="109" width="109"/>
+            </div>
+
+        <p font-size="28px">Año con año, la temporada de lluvias causa estragos en el Área Metropolitana de Guadalajara y las distintas regiones de Jalisco por tormentas tropicales y huracanes, causando daños materiales y en el peor de los casos, pérdidas humanas. No podemos detener
+            las acciones de la naturaleza, pero juntas y juntos sí podemos prevenir y aminorar los daños que ocasionan, poniendo de nuestra parte. </p>
+        <p>
+            Si vives en el Área Metropolitana de Guadalajara
+        </p>
+
+        <div style="position:absolute; top: 54px; left: 482px;">
+            <img src="../img/Grupo 160.png" height="83" width="83"/>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
